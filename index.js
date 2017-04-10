4dd98e0c27510dcc3775a419b9e2b940c1f3f596
Greg Lamp <lamp.greg@gmail.com>
2017-04-10 15:06:51 -0400

    any color you want

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var colors = require('colors');
 var Stream = require('stream').Stream;
 
-module.exports = () => {
+module.exports = (options) => {
+  var color = options.color || 'cyan';
+  if (! color in colors.styles) {
+    color = 'cyan';
+  }
+
   var colorStream = new Stream;
   colorStream.writable = true;
   colorStream.bytes = 0;
 
   colorStream.write = function(buf) {
     colorStream.bytes += buf.length;
-    process.stderr.write(buf.toString().cyan);
+    process.stderr.write(buf.toString()[color]);
   }
 
   colorStream.end = function(buf) {
